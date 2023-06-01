6f62cc9688fc86b20912d2916ddbe216f7a670e0
monelover <monelover1202@gmail.com>
2023-06-01 12:57:54 +0900

    save 해보기

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,8 +1,20 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
 
 @RestController
 public class UserController {
-
+	@Autowired
+	UserService userService;
+	@PostMapping("/user")
+	public User save(@RequestBody User reqUser) {
+		User user = userService.add(reqUser);
+		return user;
+	}
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,8 +1,17 @@
 package com.example.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
 
 @Service
 public class UserService {
+	@Autowired
+	UserRepository userRepository;
+	public User add(User reqUser) {
+		return userRepository.save(reqUser);
+	}
 
 }
